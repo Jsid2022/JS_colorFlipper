900744b835860aadec201a795367381ef56523d1
Jsid2022 <vasujain8998@gmail.com>
2022-12-03 21:24:14 +0530

    added comments to js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let color = "white";
 const colorArr = ["#9C254D", "white", "#10A19D", "#F8F988", "#453C67"]
 let type = "simple";
 
+// This function will set the Nav tabs
 function setColorType(colortype) {
     if (colortype === "simple") {
         simpleSpan.style.color = "#289ff5";
@@ -17,12 +18,17 @@ function setColorType(colortype) {
     type = colortype;
 }
 
+// This function generates the random number
 function getRandomNumber(len) {
     return Math.floor(Math.random() * len);
 }
 
+// This function will get random color from colorArr[]
 function getSimpleRandomColor() {
+    // fetch random no
     let randomNo = getRandomNumber(colorArr.length);
+    // if fetched color is same as previous one
+    // fetch color again until it is not same
     if (colorArr[randomNo] === color) {
         while (colorArr[randomNo] === color) {
             randomNo = getRandomNumber();
@@ -32,6 +38,7 @@ function getSimpleRandomColor() {
     return colorArr[randomNo];
 }
 
+// this function will fetch Random Hex code
 function getHexRandomColor() {
     const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
     let hexColor = "#";
@@ -43,6 +50,8 @@ function getHexRandomColor() {
     return hexColor;
 }
 
+// This function will change the background color
+// every time button is clicked
 function changeColor() {
     if (type == "simple") {
         color = getSimpleRandomColor();
